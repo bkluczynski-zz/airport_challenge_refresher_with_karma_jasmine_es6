953fe0b25910521ee681e6f4df58229d8df756b8
Bart <kluczynskib@gmail.com>
2017-10-04 19:12:44 +0100

    added As an air traffic controller

--- source/Airport.js
+++ source/Airport.js
@@ -1,9 +1,11 @@
 import { Weather } from './Weather'
+const CAPACITY = 20;
 
 export class Airport {
-  constructor(){
+  constructor(capacity = CAPACITY){
     this.planes = [];
     this.weather = new Weather();
+    this.capacity = capacity;
   }
   getPlanes(){
     return this.planes.length;
@@ -11,6 +13,10 @@ export class Airport {
   requestPlaneToLand(plane){
     if (this.weather.isBadWeather()){
       throw new Error("sorry, but you can't land due to bad weather")
+    }
+
+    if (this.planes.length >= this.capacity){
+      throw new Error("oh sorry, we can't land your plane at the moment, you have to divert to the nearest airport")
     }
      plane.flying = false;
      this.planes.push(plane);
